3fa630240f23bc0438493d8e92b4d0eb0d46f007
Byeori-Kim <99kbr11@gmail.com>
2021-03-01 23:06:54 +0900

    Test

---
+++ index.php
@@ -0,0 +1,155 @@
+<!-- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Berry's Homepage</title>
+<link href="bitnami.css" media="all" rel="Stylesheet" type="text/css" />
+</head>
+<body>
+<div id="container">
+  <div id="header"> 
+    <div id="bitnami">
+        <a href="/"><img alt="404 Not Found" src="img/google.png" /></a>
+    </div>
+  </div>
+    <div id="menu_launch_page">
+      <table cellpadding="0" cellspacing="0">
+        <tr>
+          <td><img src="img/tab1_welcome.png" alt="404 Not Found" /></td>
+        </tr>
+      </table>
+    </div>
+  <div id="lowerContainer">
+    <div id="content">
+        <div align="center">
+<table class="tableParagraph">
+<tr>
+<td class="container">
+<p><br/><br/>
+
+naver <a href="https://naver.com">https://naver.com</a><br/><br/><br/><br/>
+
+Youtube <a href="https://youtube.com">https://youtube.com</a><br/><br/> <br/><br/>
+
+Usaint <a href="https://saint.ssu.ac.kr">https://saint.ssu.ac.kr</a><br/><br/>
+
+</p>
+</td>
+</tr>
+</table>
+        </div>
+        <br/><br/>
+   </div>
+  </div>
+</div>
+</body>
+</html> -->
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #987654;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+// echo($name);
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[A-z]+/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+      
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+      
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
